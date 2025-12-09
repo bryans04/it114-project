1edcc753c1f3056efb1c51042c805f15740466b8
Bryan Sanchez <bryansanchez6191@outlook.com>
2025-12-08 19:00:57 -0500

    Milestone2 work

--- Project/Common/PayloadTest.java
+++
@@ -1,8 +0,0 @@
-package Project.Common;
-
-public class PayloadTest {
-    public static void main(String[] args) {
-        PointsPayload payload = new PointsPayload(12345L, 10);
-        System.out.println(payload.toString());
-    }
-}
